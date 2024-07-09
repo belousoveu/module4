e8eab52cf36a4d0a2a9f068b32d971400588c72b
Belousoveu <belousov.eu@gmail.com>
2024-07-09 16:45:16 +0300

    initial commit

---
+++ module_4_1.py
@@ -0,0 +1,8 @@
+import true_math as tm
+import fake_math as fm
+
+print(tm.divide(15, 3))
+print(tm.divide(15, 0))
+
+print(fm.divide(25, 4))
+print(fm.divide(25, 0))
---
+++ true_math.py
@@ -0,0 +1,5 @@
+from math import inf
+
+
+def divide(a, b):
+    return a / b if b != 0 else inf
